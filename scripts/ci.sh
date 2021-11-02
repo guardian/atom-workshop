ed7d79f1de8830524eaf4061797b7250524c0672
Sam Hession <samhession@live.co.uk>
2021-11-02 15:44:40 +0000

    Adds ci script

---
+++ scripts/ci.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+setupNvm() {
+    export NVM_DIR="$HOME/.nvm"
+    [[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+    nvm install
+    nvm use
+}
+
+globalJsDependencies() {
+    npm install -g yarn
+}
+
+yarnSetup() {
+    yarn install --force
+    yarn lint
+    yarn test
+    yarn build
+}
+
+riffRaffUpload() {
+    sbt clean compile riffRaffUpload
+}
+
+main() {
+    setupNvm
+    globalJsDependencies
+    yarnSetup
+    riffRaffUpload
+}
+
+main
